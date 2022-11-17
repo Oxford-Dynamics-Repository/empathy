3a62b88d39fa15331771f0ff9d3cd923df24b8e5
Johann Diep <johann@oxdynamics.com>
2022-11-17 22:05:15 +0100

    update

---
+++ scripts/converse_t2s_s2t.py
@@ -0,0 +1,48 @@
+# Johann Diep (johann@oxdynamics.com) - Oxford Dynamics - November 2022
+#
+# This script translates speech to text and text to speech.
+ 
+import speech_recognition as sr
+import pyttsx3
+ 
+
+# Function to convert text to speech.
+def text_to_speech(text):
+    engine = pyttsx3.init() # Initialize the engine.
+    engine.say(text)
+    engine.runAndWait()
+
+def speech_to_text():
+    # Initialize the recognizer.
+    recognizer = sr.Recognizer()
+        
+    # Looping infinitely for user to speak.
+    while(True):   
+        try: # Exception handling to handle exceptions at the runtime.
+            with sr.Microphone() as source:
+                # Wait for a second to let the recognizer adjust the energy threshold 
+                # based on the surrounding noise level.
+                recognizer.adjust_for_ambient_noise(source, duration=1)
+                
+                # Listening for the user input.
+                audio = recognizer.listen(source)
+                
+                # Using Google to recognize audio.
+                text = recognizer.recognize_google(audio)
+                text = text.lower()
+
+                return text
+                
+        except sr.RequestError as e:
+            print("Could not request results: {0}".format(e))
+            
+        except sr.UnknownValueError:
+            print("Unknown error occurred.")
+
+def main():
+    output = speech_to_text()
+    text_to_speech(output)
+    
+
+if __name__ == '__main__':
+    main()
