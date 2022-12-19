603b27eebb172624605a38fafd9ecb290d1d4120
Johann Diep <johann@oxdynamics.com>
2022-12-19 07:29:13 +0100

    update converse_t2s_s2t.py

--- scripts/converse_t2s_s2t.py
+++ scripts/converse_t2s_s2t.py
@@ -3,52 +3,65 @@
 # This script translates speech to text and text to speech.
  
 import speech_recognition as sr
-import pyttsx3
- 
+import playsound
+import os
+import time
+from gtts import gTTS
 
-# Function to convert text to speech.
-def text_to_speech(text):
-    engine = pyttsx3.init() # Initialize the engine.
-    voice = engine.getProperty('voices')
-    engine.setProperty('voice', voice[0].id)
-    engine.say(text)
-    engine.runAndWait()
 
-def speech_to_text():
-    # Initialize the recognizer.
-    recognizer = sr.Recognizer()
-        
-    # Looping infinitely for user to speak.
-    while(True):
-        try: # Exception handling to handle exceptions at the runtime.
-            print("Try-block called.")
+class VoiceClass:
+    def __init__(self):
+        self.recognizer = sr.Recognizer() # Initialize the recognizer.
 
-            with sr.Microphone() as source:
-                print("Feel free to speak.")
+    def text_to_speech(self, text):
+        tts = gTTS(text = text, lang = 'fr', tld = 'fr')
 
-                # Wait for a second to let the recognizer adjust the energy threshold 
-                # based on the surrounding noise level.
-                recognizer.adjust_for_ambient_noise(source, duration=0.5)
-                
-                # Listening for the user input.
-                audio = recognizer.listen(source)
-                
-                # Using Google to recognize audio.
-                text = recognizer.recognize_google(audio)
-                text = text.lower()
+        filename = 'AI_voice.mp3'
+        tts.save(filename)
+        playsound.playsound(filename)
 
-                return text
-                
-        except:
-            print("Pass-block called.")
-            pass
+        os.remove(filename)
+
+    def speech_to_text(self):
+        timeout = time.time() + 15 # 15 seconds time to ask a question
+
+        # Looping infinitely for user to speak.
+        while(True):
+            if time.time() > timeout: 
+                print("Break called.")
+                break
+
+            try: # Exception handling to handle exceptions at the runtime.
+                print("Try-block called.")
+
+                with sr.Microphone() as source:
+                    print("Feel free to speak.")
+
+                    # Wait for a second to let the recognizer adjust the energy threshold 
+                    # based on the surrounding noise level.
+                    self.recognizer.adjust_for_ambient_noise(source, duration=1)
+
+                    # Listening for the user input.
+                    audio = self.recognizer.listen(source)
+                    
+                    # Using Google to recognize audio.
+                    text = self.recognizer.recognize_google(audio)
+                    text = text.lower()
+
+                    return text
+                    
+            except:
+                print("Pass-block called.")
+                pass
 
 def main():
+    VoiceObject = VoiceClass()
+
     i = 0
-    while(i < 2):
-        output = speech_to_text()
+    while(i < 1):
+        output = VoiceObject.speech_to_text()
         print("Your text: " + output)
-        text_to_speech(output)
+        VoiceObject.text_to_speech(output)
 
         i = i + 1
 
