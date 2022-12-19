737746897e09a528b89f5c1013779eaf811dbb97
Johann Diep <johann@oxdynamics.com>
2022-12-19 07:46:23 +0100

    updaet

--- scripts/test.py
+++
@@ -1,6 +0,0 @@
-from transformers import EncoderDecoderModel, BertTokenizer
-
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-model = EncoderDecoderModel.from_encoder_decoder_pretrained("google/tapas-base", "bert-base-cased")
-
-
